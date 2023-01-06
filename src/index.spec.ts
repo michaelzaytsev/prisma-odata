aff1cbe1be168603dfdc198667a04bfef4af471c
Michael Zaytsev <michael.zaytsev.world@outlook.com>
2023-01-06 03:30:29 -0800

    Added string methods handle

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,13 @@
 import { parseODataFilter } from '.';
+import each from 'jest-each';
 
 describe('parseODataFilter', () => {
   test('and', () => {
     expect(parseODataFilter('property lt 12 and model/property gt 7')).toStrictEqual({
-      property: { lt: 12 },
-      model: { property: { gt: 7 }},
+      AND: [
+        { property: { lt: 12 }},
+        { model: { property: { gt: 7 }}},
+      ],
     });
   });
 
@@ -17,17 +20,55 @@ describe('parseODataFilter', () => {
     });
   });
 
-  test('not', () => {
-    expect(parseODataFilter(`property1 ne null and property2 ne 'null'`)).toStrictEqual({
-      NOT: {
-        property1: null,
-        property2: 'null',
-      },
+  test('ne', () => {
+    expect(parseODataFilter(`property ne null`)).toStrictEqual({ NOT: { property: null }});
+  });
+
+  test('ne and ne for the same property', () => {
+    expect(parseODataFilter(`property ne null and property ne 'null'`)).toStrictEqual({
+      AND: [
+        { NOT: { property: null }},
+        { NOT: { property: 'null' }},
+      ],
     });
   });
 
   test('eq', () => {
-    expect(parseODataFilter(`property1 eq null and property2 eq 'null'`))
-      .toStrictEqual({ property1: null, property2: 'null' });
+    expect(parseODataFilter(`property eq 12`)).toStrictEqual({ property: 12 });
+  });
+
+  each([
+    ['lt', 'lt'],
+    ['gt', 'gt'],
+    ['le', 'lte'],
+    ['ge', 'gte'],
+  ])
+    .test('%s', (odataOperator, prismaOperator) => {
+      expect(parseODataFilter(`property ${odataOperator} 12`)).toStrictEqual({ property: { [prismaOperator]: 12 }});
+    });
+
+  each([
+    ['contains', 'contains'],
+    ['startswith', 'startsWith'],
+    ['endswith', 'endsWith'],
+  ])
+    .test('%s', (odataMethod, prismaMethod) => {
+      expect(parseODataFilter(`${odataMethod}(property, 'substring')`))
+        .toStrictEqual({ property: { [prismaMethod]: 'substring' }});
+    });
+
+  test('not contains', () => {
+    expect(parseODataFilter(`not contains(property, 'substring')`))
+      .toStrictEqual({ NOT: { property: { contains: 'substring' }}});
+  });
+
+  test('not contains or not contains for the same property', () => {
+    expect(parseODataFilter(`not contains(property, 'substring1') or not contains(property, 'substring2')`))
+      .toStrictEqual({
+        OR: [
+          { NOT: { property: { contains: 'substring1' }}},
+          { NOT: { property: { contains: 'substring2' }}},
+        ],
+      });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -13,28 +13,38 @@ export interface ParsedODataQuery {
 }
 
 function convertOperation(operation: any[], binds: ODataBinds): any {
-  const operator = operation[0].toLowerCase();
+  const operator = operation[0];
   switch (operator) {
     case 'and':
-      return operation.slice(1).reduce((result, suboperation) => {
-        const convertedSuboperation = convertOperation(suboperation, binds);
-        if (suboperation[0].toLowerCase() === 'ne' && result.NOT) {
-          Object.assign(result.NOT, convertedSuboperation.NOT);
-          return result;
-        } else {
-          return Object.assign(result, convertedSuboperation);
-        }
-      }, {});
+      return { AND: operation.slice(1).map(suboperation => convertOperation(suboperation, binds)) };
     case 'or':
       return { OR: operation.slice(1).map(suboperation => convertOperation(suboperation, binds)) };
+    case 'not':
+      return { NOT: convertOperation(operation[1], binds) };
     case 'ne':
       return { NOT: composeProperty(operation[1], operation[2] === null ? null : binds[operation[2].bind][1]) };
     case 'eq':
       return composeProperty(operation[1], operation[2] === null ? null : binds[operation[2].bind][1]);
     case 'lt': case 'gt':
       return composeProperty(operation[1], { [operator]: binds[operation[2].bind][1] });
+    case 'le':
+      return composeProperty(operation[1], { lte: binds[operation[2].bind][1] });
+    case 'ge':
+      return composeProperty(operation[1], { gte: binds[operation[2].bind][1] });
+    case 'call':
+      const { method, args } = operation[1];
+      switch (method) {
+        case 'contains':
+          return composeProperty(args[0], { contains: binds[args[1].bind][1] });
+        case 'startswith':
+          return composeProperty(args[0], { startsWith: binds[args[1].bind][1] });
+        case 'endswith':
+          return composeProperty(args[0], { endsWith: binds[args[1].bind][1] });
+        default:
+          throw new Error(`Unhandled method "${method}"`);
+      }
     default:
-      throw new Error(`Unhandled operator "${operator.toUpperCase()}"`);
+      throw new Error(`Unhandled operator "${operator}"`);
   }
 }
 
